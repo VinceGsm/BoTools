ee0a680d96a06e11b5d8932eb4044827c098f493
VinceGusmini <vincent.gusmini.sio@gmail.com>
2021-03-29 19:25:07 +0200

    connection to server ok

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Discord;
+using Discord.WebSocket;
 using log4net;
 using log4net.Config;
 using System;
@@ -11,16 +12,29 @@ namespace BoTools
 {
 	public class Program
 	{
+		private DiscordSocketClient _client;
+		private string _token = Environment.GetEnvironmentVariable("BoTools_Token");
 		private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
 		public static void Main(string[] args) 
 			=> new Program().MainAsync().GetAwaiter().GetResult();
 
+
 		public async Task MainAsync()
         {
             LoadLogConfig();
 
+            _client = new DiscordSocketClient(); 
+            //TODO : msg latency    
+			_client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
         }
+
 
         private static void LoadLogConfig()
         {            
@@ -31,7 +45,27 @@ namespace BoTools
 
         private Task Log(LogMessage msg)
 		{
-			log.Info(msg.ToString());
+            //TODO : service ?
+            
+            switch (msg.Severity)
+            {
+                case LogSeverity.Critical:
+                case LogSeverity.Error:
+                    log.Error(msg.Message);
+                    break;
+                
+                case LogSeverity.Warning:
+                    log.Warn(msg.Message);
+                    break;
+
+                case LogSeverity.Verbose:
+                    log.Debug(msg.Message);
+                    break;
+
+                default:
+                    log.Info(msg.Message);
+                    break;
+            }			
 			return Task.CompletedTask;
 		}
 	}
