fca3e215e02af48b778f383736c3e3dfd715c55b
VinceGsm <v.gusmini@outlook.fr>
2023-01-25 17:31:53 +0100

    fix GetNextNumOnePiece

--- Service/EventService.cs
+++ Service/EventService.cs
@@ -7,7 +7,6 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Reflection;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BoTools.Service
@@ -33,7 +32,7 @@ namespace BoTools.Service
             {
                 var name = $"One Piece {GetNextNumOnePiece()} Streaming";
                 DateTime target = Helper.GetNextWeekday(DateTime.Today, DayOfWeek.Sunday);
-                DateTimeOffset startTime = new DateTimeOffset(target.AddHours(21.2));   // 21h12
+                DateTimeOffset startTime = new DateTimeOffset(target.AddHours(21));   // 21h
                 GuildScheduledEventType type = GuildScheduledEventType.Voice;
                 string description = "**RDV hebdomadaire du server !**";
                 ulong? channelId = Helper._idSaloonVoice;
@@ -54,22 +53,16 @@ namespace BoTools.Service
             {
                 using (HttpClient httpClient = new HttpClient())
                 {
-                    html = httpClient.GetStringAsync("https://www.vostfr-episode.com/anime-one-piece").Result;
+                    html = httpClient.GetStringAsync("https://www.themoviedb.org/tv/37854/episode_group/5ad0f096c3a36825a300e78b/group/5ad0f3230e0a260942006562").Result;
                 }
                 htmlDoc.LoadHtml(html);
-
-                string xpathBeforeTarget = "/html[1]/body[1]/div[2]/div[4]/div[6]/div[1]/div[1]/ul[1]/li[21]/a[1]";
-                var listNode = htmlDoc.DocumentNode.Descendants("a").ToList();
-                var beforeTargetNode = htmlDoc.DocumentNode.SelectSingleNode(xpathBeforeTarget);
-
-                var target = listNode[listNode.IndexOf(beforeTargetNode) + 1].InnerText.Trim();
-
-                var season = Regex.Match(target, @"\d+").Value;
-                int num = Int32.Parse(target.Substring(13, 4));
-
-                return num + 1;
+                
+                var nbEpReleased = htmlDoc.DocumentNode.SelectNodes("//span[contains(@class, 'episode_number')]").Count;
+                
+                return nbEpReleased + 2; //check if regular
             }
-            catch(Exception ex) { return 0; }
+            catch(Exception ex) 
+            { log.Error(ex.Message); return 0; }
         }
 
         public async void CreateEventSeries(string name, int numFirstEpisode, int nbEp, DayOfWeek dayOfWeek, Double hour)
