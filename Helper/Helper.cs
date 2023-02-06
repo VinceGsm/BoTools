4217787c5a24c8fe54e93ca41eb499bd276f6103
VinceGsm <v.gusmini@outlook.fr>
2023-02-06 14:48:21 +0100

    fix IMG Jellyfin (no white bg)

--- Helper/Helper.cs
+++ Helper/Helper.cs
@@ -14,8 +14,8 @@ namespace BoTools
     {
         public static readonly string statusLink = "https://www.twitch.tv/vince_zder";
         public static readonly string _zderLandIconUrl = "https://cdn.discordapp.com/attachments/494958624922271745/1056847373436977162/brookByVince.gif";        
-        public static readonly string _JellyfinImgUrl = 
-            "https://cdn.discordapp.com/attachments/617462663374438411/1072134080667336724/Vincezder_mesmering_television_detailed_colourful_0_4b6a0db4-18ea-4011-a1db-fadf4e8ce347.png";
+        public static readonly string _JellyfinImgUrl =
+            "https://cdn.discordapp.com/attachments/617462663374438411/1072151541638770698/Vincezder_mesmering_television_detailed_colourful_0_4b6a0db4-18ea-4011-a1db-fadf4e8ce347-NoWhite.png";
         public static readonly string _boToolsGif = "https://cdn.discordapp.com/attachments/553256709439750151/1062431704914067566/KatakuriLow.gif";
         public static readonly string _urlAvatarVince = "https://cdn.discordapp.com/attachments/617462663374438411/846821971114983474/luffy.gif";
         public static readonly ulong _ZderLandId = 312966999414145034;
